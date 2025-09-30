b67e6790bcf2a17929b9f7faae2ecc9142052718
janwisniak <janwisniak2017@gmail.com>
2025-09-30 20:08:11 +0200

    commit1

---
+++ zadanieThemes/app/src/main/java/com/example/zadaniethemes/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.zasobywjava;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.zadaniethemes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        String errorMessage = getResources().getString(R.string.error_message);
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+    }
